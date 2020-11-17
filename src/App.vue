f849f89fce411caec0a05b7f4e16e1a56214f6cd
Azam2926 <azam.inoyatov.1998@gmail.com>
2020-11-18 02:28:54 +0500

    built for iazam.me

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,54 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app>
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            v-bind:src="'https://cdn.vuetifyjs.com/images/logos/vuetify-logo-'+ theme +'.png'"
+            transition="scale-transition"
+            width="40"
         />
 
         <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+            alt="Vuetify Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            v-bind:src="'https://cdn.vuetifyjs.com/images/logos/vuetify-name-'+ theme +'.png'"
+            width="100"
         />
       </div>
-
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn @click="toggleTheme">
+        <v-icon>mdi-invert-colors</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-main>
       <HelloWorld/>
     </v-main>
+
+    <v-footer
+        padless
+        app
+    >
+      <v-card
+          class="flex"
+          flat
+          tile
+      >
+        <v-card-text class="py-2 text-center">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
@@ -54,7 +58,17 @@ export default {
   },
 
   data: () => ({
-    //
+    theme: 'dark',
   }),
-};
+
+  methods: {
+    toggleTheme: function () {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+      if (this.$vuetify.theme.dark)
+        this.theme = 'dark'
+      else
+        this.theme = 'light'
+    },
+  },
+}
 </script>
