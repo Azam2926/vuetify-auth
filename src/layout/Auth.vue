eed408ce6282ecbd8d2ba841d39c586108fe0718
Azam2926 <azam.inoyatov.1998@gmail.com>
2020-11-19 02:03:08 +0500

    can change theme for Auth.vue layout

--- src/layout/Auth.vue
+++ src/layout/Auth.vue
@@ -12,6 +12,9 @@
       >
         <v-card-text class="py-2 text-center">
           {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+          <v-btn class="float-right" fab small @click="toggleTheme">
+            <v-icon>mdi-invert-colors</v-icon>
+          </v-btn>
         </v-card-text>
       </v-card>
     </v-footer>
@@ -21,8 +24,51 @@
 <script>
 
 export default {
-  name: 'Main',
+  name: 'Auth',
+  data: () => ({
+    theme: ''
+  }),
+  methods: {
+    toggleTheme: function () {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+      if (this.$vuetify.theme.dark)
+        this.changeTheme('dark')
+      else
+        this.changeTheme('light')
+    },
+
+    /**
+     * @param {string} tema for theme param
+     */
+    changeTheme (tema) {
+      this.theme = tema
+      localStorage.setItem('theme', tema)
+    },
+  },
+
+  created () {
+    if (localStorage.getItem('theme'))
+      this.theme = localStorage.getItem('theme')
+    else if (window.matchMedia('(prefers-color-scheme: dark)').matches)
+      this.changeTheme('dark')
+    else
+      this.changeTheme('light')
+
+    this.$vuetify.theme.dark = this.theme === 'dark'
+
+    window.matchMedia('(prefers-color-scheme: dark)').addListener((e) => {
+      if (e.matches)
+        this.changeTheme('dark')
+      else
+        this.changeTheme('light')
+      this.$vuetify.theme.dark = this.theme === 'dark'
+
+      console.log(`changed to ${e.matches ? 'dark' : 'light'} mode`)
+    })
+  },
 }
+
+
 </script>
 
 <style scoped>
